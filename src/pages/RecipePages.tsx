aa53269967f3f902c1a5e60d13f213c512e13ba8
Dasar0410 <danielsarjomaa@gmail.com>
2026-03-25 13:47:49 +0100

    added robots.txt and helmet to recipePages

--- src/pages/RecipePages.tsx
+++ src/pages/RecipePages.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom'
+import { Helmet } from 'react-helmet-async'
 import NavigationBar from '../components/NavigationBar'
 import TitleInstructionCards from '../components/TitleInstructionCard'
 import IngredientsCard from '../components/IngredientsCard'
@@ -26,6 +27,17 @@ function RecipePages() {
 
     return (
         <>
+            {recipeData && (
+                <Helmet>
+                    <title>{recipeData.title} — Sarjomaa Cuisine</title>
+                    <meta name="description" content={recipeData.description} />
+                    <meta property="og:title" content={recipeData.title} />
+                    <meta property="og:description" content={recipeData.description} />
+                    <meta property="og:image" content={recipeData.image_url} />
+                    <meta property="og:url" content={`https://sarjomaa.no/oppskrifter/${recipeData.slug}`} />
+                    <link rel="canonical" href={`https://sarjomaa.no/oppskrifter/${recipeData.slug}`} />
+                </Helmet>
+            )}
             <div className=''><NavigationBar/></div>
             
             <div className=' flex flex-row flex-wrap justify-center'>
