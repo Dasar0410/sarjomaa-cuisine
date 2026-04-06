310c261c8ab0132e39442b9f371b12d67f52c4b4
Dasar0410 <danielsarjomaa@gmail.com>
2026-04-07 01:36:18 +0200

    added boneyard and updated react query parameters

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App.tsx'
 import './index.css'
+import './bones/registry'
 import { AuthContextProvider } from './context/AuthContext.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
